139fb7072ca2229166ee9ad750094fda0862b1cf
Charlie K <issyrocks2020@gmail.com>
2017-08-07 06:06:09 +0000

    Update PreCommit.php

--- scripts/PreCommit.php
+++ scripts/PreCommit.php
@@ -1,7 +1,6 @@
 <?php
 
 exec('git diff --cached --name-status --diff-filter=ACM', $output);
-exec('cd ../');
 foreach ($output as $file) {
     $fileName = '../'.trim(substr($file, 1));
     /**
@@ -18,12 +17,12 @@ foreach ($output as $file) {
             /*
              * PHP-CS-Fixer && add it back
              */
-            echo 'Checking psr-2 conformity of '.trim(substr($file, 1))."\n";
+            echo 'Checking PSR-2 & Symfony conformity of '.trim(substr($file, 1))."\n";
             exec("php-cs-fixer fix {$fileName} --rules=@PSR2,@Symfony --using-cache=no");
             exec("git add {$fileName}");
         } else {
-            echo "\nYour commit has php syntax error(s).\nYou MUST fix it before commit.\n";
-            echo "See 'php -l' error massage below.\n\n---------------------";
+            echo "\nYour commit has php syntax error(s).\nYou MUST fix them before you commit.\n";
+            echo "See the error massage(s) below.\n\n---------------------";
             echo implode("\n", $lint_output), "\n---------------------\n";
             exit(1);
         }
