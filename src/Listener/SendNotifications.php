d9929664336bb374ccd15db376cfa1d62112f831
the-turk <hasanoozbey@gmail.com>
2020-02-26 18:35:09 +0300

    notification fix

--- src/Listener/SendNotifications.php
+++ src/Listener/SendNotifications.php
@@ -53,7 +53,7 @@ class SendNotifications
      */
     public function sync(Post $post, User $user, $reaction, array $recipients)
     {
-        if ($post->user->id != $user->id) {
+        if (null !== $post->user && $post->user->id != $user->id) {
             app(NotificationSyncer::class)->sync(
                 new PostReactedBlueprint($post, $user, $reaction),
                 $recipients
