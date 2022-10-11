1d95c0476b5f4bef8dfd3231e72cf8079ed58410
Sami Mazouz <sychocouldy@gmail.com>
2022-10-11 13:42:47 +0100

    fix: migrations assume flarum is already installed

--- migrations/2019_07_08_000003_migrate_extension_settings.php
+++ migrations/2019_07_08_000003_migrate_extension_settings.php
@@ -13,18 +13,14 @@ use Illuminate\Database\Schema\Builder;
 
 return [
     'up' => function (Builder $schema) {
-        /**
-         * @var \Flarum\Settings\SettingsRepositoryInterface
-         */
-        $settings = resolve('flarum.settings');
+        $db = $schema->getConnection();
 
         $keys = ['convertToUpvote', 'convertToDownvote', 'convertToLike'];
 
         foreach ($keys as $key) {
-            if ($value = $settings->get($full = "reflar.reactions.$key")) {
-                $settings->set("fof-reactions.$key", $value);
-                $settings->delete($full);
-            }
+            $db->table('settings')
+                ->where('key', "reflar.reactions.$key")
+                ->update(['key' => "fof-reactions.$key"]);
         }
     },
 ];
