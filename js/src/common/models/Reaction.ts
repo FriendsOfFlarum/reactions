2d2f390eb59aac7fceab16a6759cf0a8ef1d336a
Ian Morland <ian@morland.me>
2023-06-20 21:41:44 +0100

    add missing call()

--- js/src/common/models/Reaction.ts
+++ js/src/common/models/Reaction.ts
@@ -2,30 +2,30 @@ import Model from 'flarum/common/Model';
 
 export default class Reaction extends Model {
   identifier() {
-    return Model.attribute<string>('identifier');
+    return Model.attribute<string>('identifier').call(this);
   }
 
   display() {
-    return Model.attribute<string>('display');
+    return Model.attribute<string>('display').call(this);
   }
 
   type() {
-    return Model.attribute<string>('type');
+    return Model.attribute<string>('type').call(this);
   }
 
   enabled() {
-    return Model.attribute<boolean>('enabled');
+    return Model.attribute<boolean>('enabled').call(this);
   }
 
   user_id() {
-    return Model.attribute<number>('user_id');
+    return Model.attribute<number>('user_id').call(this);
   }
 
   post_id() {
-    return Model.attribute<number>('post_id');
+    return Model.attribute<number>('post_id').call(this);
   }
 
   reaction_id() {
-    return Model.attribute<number>('reaction_id');
+    return Model.attribute<number>('reaction_id').call(this);
   }
 }
--- js/src/forum/models/PostReaction.ts
+++ js/src/forum/models/PostReaction.ts
@@ -5,26 +5,26 @@ import Reaction from 'src/common/models/Reaction';
 
 export default class PostReaction extends Model {
   userId() {
-    return Model.attribute<number>('userId');
+    return Model.attribute<number>('userId').call(this);
   }
 
   postId() {
-    return Model.attribute<number>('postId');
+    return Model.attribute<number>('postId').call(this);
   }
 
   reactionId() {
-    return Model.attribute<number>('reactionId');
+    return Model.attribute<number>('reactionId').call(this);
   }
 
   user() {
-    return Model.hasOne<User>('user');
+    return Model.hasOne<User>('user').call(this);
   }
 
   post() {
-    return Model.hasOne<Post>('post');
+    return Model.hasOne<Post>('post').call(this);
   }
 
   reaction() {
-    return Model.hasOne<Reaction>('reaction');
+    return Model.hasOne<Reaction>('reaction').call(this);
   }
 }
