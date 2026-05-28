779c2c530c270d287d868852eb311d2390419665
David Sevilla Martin <me@dsevilla.dev>
2026-05-27 22:26:19 -0400

    fix: ensure event dispatching only occurs for existing post reactions
    This resolves a 500 caused by the PostWasUnreacted constructor receiving null instead of a valid post reaction. If "unreacting" didn't actually take place (safe operation to ignore if not needed), then we shouldn't fire said event.

--- src/PostResourceFields.php
+++ src/PostResourceFields.php
@@ -163,6 +163,9 @@ class PostResourceFields
             } else {
                 $guestId = $context->request->getAttribute('session')?->getId();
 
+                /**
+                 * @var PostReaction|PostAnonymousReaction|null $postReaction
+                 */
                 if ($actor->isGuest()) {
                     $postReaction = PostAnonymousReaction::where([['guest_id', $guestId], ['post_id', $post->id]])->first();
                 } else {
@@ -180,16 +183,15 @@ class PostResourceFields
 
                         $postReaction->reaction_id = null;
                         $postReaction->save();
-                    }
 
-                    $this->events->dispatch(new PostWasUnreacted($post, $postReaction, $actor));
+                        // Only dispatch event if a post reaction actually existed (anonymous or not).
+                        // We can treat this as a no-op since the end result is the same as if a reaction existed and had been removed.
+                        $this->events->dispatch(new PostWasUnreacted($post, $postReaction, $actor));
+                    }
                 } else {
                     $this->validateReaction($reaction, $reactionId);
 
-                    if ($postReaction) {
-                        $postReaction->reaction_id = $reaction->id;
-                        $postReaction->save();
-                    } else {
+                    if (!$postReaction) {
                         $isGuest = $actor->isGuest();
                         $postReaction = $isGuest ? new PostAnonymousReaction() : new PostReaction();
 
@@ -200,11 +202,10 @@ class PostResourceFields
                         } else {
                             $postReaction->user_id = $actor->id;
                         }
-
-                        $postReaction->reaction_id = $reaction->id;
-
-                        $postReaction->save();
                     }
+
+                    $postReaction->reaction_id = $reaction->id;
+                    $postReaction->save();
 
                     // We'll maintain current behaviour and only push to Pusher if the reaction is not anonymous
                     if ($postReaction instanceof PostReaction) {
