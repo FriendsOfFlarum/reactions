20105ae0e71f1bc5809554d567c5bfb903d5f2eb
David Sevilla Martin <me@dsevilla.dev>
2026-06-01 12:59:04 -0400

    fix: avoid error if foreign keys already exist when recreating

--- migrations/2026_03_22_000000_add_indexes_to_reaction_tables.php
+++ migrations/2026_03_22_000000_add_indexes_to_reaction_tables.php
@@ -30,18 +30,22 @@ return [
     },
 
     'down' => static function (Builder $schema) {
-        $schema->table('post_reactions', function (Blueprint $table) {
+        $schema->table('post_reactions', function (Blueprint $table) use ($schema) {
             // Recreate single-column foreign-key index (post_id foreign index) that was automatically dropped by DB when
             // the multi-column indexes were created. Otherwise, we hit a FK constraint error.
-            $table->index(['post_id'], 'post_reactions_post_id_foreign');
+            if (! $schema->hasIndex('post_reactions', $foreignIndex = 'post_reactions_post_id_foreign')) {
+                $table->index(['post_id'], $foreignIndex);
+            }
 
             $table->dropIndex('post_reactions_post_id_user_id_index');
             $table->dropIndex('post_reactions_post_id_reaction_id_index');
         });
 
-        $schema->table('post_anonymous_reactions', function (Blueprint $table) {
-            // Same as above
-            $table->index(['post_id'], 'post_anonymous_reactions_post_id_foreign');
+        $schema->table('post_anonymous_reactions', function (Blueprint $table) use ($schema) {
+            // Same as above, but for anonymous reactions.
+            if (! $schema->hasIndex('post_anonymous_reactions', $foreignIndex = 'post_anonymous_reactions_post_id_foreign')) {
+                $table->index(['post_id'], $foreignIndex);
+            }
 
             $table->dropIndex('post_anonymous_reactions_post_id_guest_id_index');
             $table->dropIndex('post_anonymous_reactions_post_id_reaction_id_index');
